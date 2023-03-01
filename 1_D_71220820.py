355874df686dc9b5754cad3d1e19595dd4385377
agnesiayolanda <117900996+agnesiayolandaaa@users.noreply.github.com>
2023-03-01 19:03:12 +0700

    Add files via upload

---
+++ 1_D_71220820.py
@@ -0,0 +1,9 @@
+def aritmatika(a, b, c):
+    return (c/2)*(2*a + (c-1)*b)
+
+print ('==================== BARIS ARITMATIKA ====================')
+a = int(input("Masukkan bilangan awal : "))
+b = int(input("Masukkan selisih bilangan : "))
+c = int(input("Masukkan banyaknya suku : "))
+
+print("Total dari deret aritmatika tersebut adalah:", aritmatika(a, b, c))
